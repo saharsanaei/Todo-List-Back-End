ca40fbfdef022679c585635319602625930e82bb
Faran7979 <fari79amiri@gmail.com>
2024-07-26 20:50:22 +0330

    delete token

--- src/models/User.js
+++ src/models/User.js
@@ -1,15 +1,15 @@
 import pool from '../core/configs/database.js';
 
-const createUser = async (username, password, email, token) => {
+const createUser = async (username, password, email) => {
     const result = await pool.query(
-        'INSERT INTO "User" (username, password, email, token) VALUES ($1, $2, $3, $4) RETURNING user_id, username, token',
-        [username, password, email, token]
+        'INSERT INTO "User" (username, password, email) VALUES ($1, $2, $3) RETURNING user_id, username',
+        [username, password, email]
     );
     return result.rows[0];
 };
 
 const findUserByUsername = async (username) => {
-    const result = await pool.query('SELECT user_id, username, password, token FROM "User" WHERE username = $1', [username]);
+    const result = await pool.query('SELECT user_id, username, password FROM "User" WHERE username = $1', [username]);
     return result.rows[0];
 };
 
