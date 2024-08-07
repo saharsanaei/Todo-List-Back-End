f43d3a66c941b5cd888b261c4da7986fd1488b86
Sahar <sanaie.sahar@gmail.com>
2024-08-07 11:35:24 +0330

    add router put for update category

--- src/modules/categories/routes.js
+++ src/modules/categories/routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createCategory, getCategories, deleteCategory } from './categoryController.js';
+import { createCategory, getCategories, deleteCategory, updateCategory } from './categoryController.js';
 import authenticateToken from '../../core/middlewares/authenticateToken.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/', authenticateToken, createCategory);
 router.get('/', authenticateToken, getCategories);
 router.delete('/:id', authenticateToken, deleteCategory);
+router.put('/:id', authenticateToken, updateCategory);
 
 export default router;
