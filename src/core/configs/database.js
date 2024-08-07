8b1aee3ca5cd29e7476d6a8fba0501694cd497dc
Faran7979 <fari79amiri@gmail.com>
2024-08-07 14:58:55 +0330

    adding more console errors

--- src/core/configs/database.js
+++ src/core/configs/database.js
@@ -14,5 +14,17 @@ const pool = new Pool({
         rejectUnauthorized: false
     }
 });
+pool.on('error', (err) => {
+    console.error('Unexpected error on idle client', err);
+    process.exit(-1);
+});
+
+pool.query('SELECT NOW()', (err, res) => {
+    if (err) {
+        console.error('Error connecting to the database', err);
+    } else {
+        console.log('Successfully connected to the database');
+    }
+});
 
 export default pool;
