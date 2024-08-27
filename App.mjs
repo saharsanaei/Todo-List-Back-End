526fcef36581af8763e273253b1fa45e7e087e43
Sahar <sanaie.sahar@gmail.com>
2024-08-27 14:14:42 +0330

    port 8080

--- App.mjs
+++ App.mjs
@@ -10,11 +10,14 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.CORS_ORIGIN
-}));
+// app.use(cors({
+//   origin: 'http://localhost:8081',
+//   credentials: true
+// }));
 
-const port = process.env.PORT || 3000;
+app.use(cors());
+
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use('/categories', categoriesRouter);
